3fd20fadbefbe0cd4472f650392b9a080748688c
yazaldefilimonepinto <78354432+yazaldefilimonepinto@users.noreply.github.com>
2021-03-22 17:34:56 +0200

    Update and rename public/js/Data.js to público/js/Data.js

--- público/js/Data.js
+++ público/js/Data.js
@@ -4,13 +4,13 @@ try{
             musica:"lil_Tecca-Ranson.mp3",
             artista:"Lil Tecca",
             titulo: "Ranson",
-            imagem: "/viewsplay/audio2.png"
+            imagem: "public/viewsplay/audio2.png"
         },
         {
             musica:"Lewis_capaldi.mp3",
             artista:"Lewis capaldi",
             titulo: "Before You Do",
-            imagem: "/viewsplay/audio1.png"
+            imagem: "public/viewsplay/audio1.png"
         }
     ]
 }catch(erro){
